9b644c0545b2c443fc2bcd351509dfbc03753396
Mohammad Ebrahimi <moh.ebrahimi2000@gmail.com>
2022-08-29 16:03:48 +0430

    Initial commit

---
+++ routes/index.tsx
@@ -0,0 +1,13 @@
+/** @jsx h */
+import { h } from "preact";
+
+export default function Home() {
+  return (
+    <div>
+      <p>
+        Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
+        file, and refresh.
+      </p>
+    </div>
+  );
+}
